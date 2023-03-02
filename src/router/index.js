eba9fdd065812e561521c9cbcd639131949c79d4
Kite <2914389317@qq.com>
2023-03-03 03:39:43 +0800

    Fix: update router file to fix router in production

--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,20 @@ import Advance from '../pages/advance'
 import Events from '../pages/events'
 import {Navigate} from "react-router-dom";
 
+function getPath(path) {
+    return process.env.NODE_ENV === 'development' ? path : process.env.PUBLIC_URL + path
+}
 const routes = [
     {
-        path: '/',
+        path: getPath('/'),
         element: <Navigate to="main/"/>
     },
     {
-        path: '/advance',
+        path: getPath('/advance'),
         element: <Advance/>
     },
     {
-        path: '/events',
+        path: getPath('/events'),
         element: <Events/>
     }
 ]
